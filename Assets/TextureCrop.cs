306db8104991c3c5ef84ba0410cd74a94470c3d1
Aidan <29628351+aidan-ferguson@users.noreply.github.com>
2022-08-31 21:09:41 +0100

    no longer changing uv's to 'cut out screen' and fov calculations changed

--- Assets/TextureCrop.cs
+++ Assets/TextureCrop.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class TextureCrop : MonoBehaviour
 {
-    public Material mat;
+    public Material crop_material;
 
-    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    void OnRenderImage(RenderTexture src, RenderTexture dst)
     {
         GameObject screen = GameObject.Find("PerspectiveScreen");
 
@@ -29,19 +29,47 @@ public class TextureCrop : MonoBehaviour
         Vector2 top_right_uv = new Vector2(top_right_screen.x/width, top_right_screen.y/height);
         Vector2 bottom_left_uv = new Vector2(bottom_left_screen.x/width, bottom_left_screen.y/height);
         Vector2 bottom_right_uv = new Vector2(bottom_right_screen.x/width, bottom_right_screen.y/height);
+        
+        Vector4[] corrected_uvs = { new Vector4(top_left_uv.x, top_left_uv.y, 0.0f, 0.0f),
+                                    new Vector4(top_right_uv.x, top_right_uv.y, 0.0f, 0.0f),
+                                    new Vector4(bottom_left_uv.x, bottom_left_uv.y, 0.0f, 0.0f),
+                                    new Vector4(bottom_right_uv.x, bottom_right_uv.y, 0.0f, 0.0f)};
 
-        Vector4[] corrected_uvs = { new Vector4(bottom_left_uv.x, bottom_left_uv.y, 0.0f, 0.0f),
-                                    new Vector4(top_left_uv.x, top_left_uv.y, 0.0f, 0.0f),
-                                    new Vector4(bottom_right_uv.x, bottom_right_uv.y, 0.0f, 0.0f),
-                                    new Vector4(top_right_uv.x, top_right_uv.y, 0.0f, 0.0f)};
+        // top left -> top right -> bottom right -> bottom left
+        // thank you https://math.stackexchange.com/questions/3037040/normalized-coordinate-of-point-on-4-sided-concave-polygon and in shader
+        float x0 = top_left_uv.x;
+        float y0 = top_left_uv.y;
+        float x1 = top_right_uv.x;
+        float y1 = top_right_uv.y;
+        float x2 = bottom_right_uv.x;
+        float y2 = bottom_right_uv.y;
+        float x3 = bottom_left_uv.x;
+        float y3 = bottom_left_uv.y;
 
-        Debug.Log("uvs");
-        Debug.Log(top_left_uv);
-        Debug.Log(top_right_uv);
-        Debug.Log(bottom_left_uv);
-        Debug.Log(bottom_right_uv);
+        float dx1 = x1 - x2;
+        float dx2 = x3 - x2;
+        float dx3 = x0 - x1 + x2 - x3;
+        float dy1 = (y1 - y2);
+        float dy2 = (y3 - y2);
+        float dy3 = (y0 - y1 + y2 - y3);
+        
+        float a13 = (dx3 * dy2 - dy3 * dx2) / (dx1 * dy2 - dy1 * dx2);
+        float a23 = (dx1 * dy3 - dy1 * dx3) / (dx1 * dy2 - dy1 * dx2);
+        float a11 = x1 - x0 + a13 * x1;
+        float a21 = x3 - x0 + a23 * x3;
+        float a31 = x0;
+        float a12 = y1 - y0 + a13 * y1;
+        float a22 = y3 - y0 + a23 * y3;
+        float a32 = y0;
 
-        mat.SetVectorArray("_corrected_uvs", corrected_uvs);
-        Graphics.Blit(src, dest);
+        Matrix4x4 distortion_mat = Matrix4x4.identity;
+        distortion_mat.SetRow(0, new Vector4(a11, a12, a13, 0.0f));
+        distortion_mat.SetRow(1, new Vector4(a21, a22, a23, 0.0f));
+        distortion_mat.SetRow(2, new Vector4(a31, a32, 1.0f, 0.0f));
+
+        crop_material.SetVectorArray("_perspective_screen_corners", corrected_uvs);
+        crop_material.SetMatrix("_screen_distortion_matrix", distortion_mat);
+
+        Graphics.Blit(src, dst, crop_material);
     }
 }
--- Assets/TextureCropShader.shader
+++ Assets/TextureCropShader.shader
@@ -17,7 +17,8 @@ Shader "Custom/TextureCropShader"
 
             #include "UnityCG.cginc"
 
-            float4 _corrected_uvs[4]; 
+            float4 _perspective_screen_corners[4]; 
+            float4x4 _screen_distortion_matrix;
 
             struct appdata
             {
@@ -34,32 +35,22 @@ Shader "Custom/TextureCropShader"
             sampler2D _MainTex;
             float4 _MainTex_ST;
             
-            v2f vert (appdata v)
+            v2f vert(appdata v)
             {
                 v2f o;
+                o.uv = v.uv;
                 o.vertex = UnityObjectToClipPos(v.vertex);
-                // Very hacky, find a way to fix at some point
-                if(v.uv.x == 0.0f && v.uv.y == 0.0f){
-                    // Bottom left
-                    o.uv = float2(_corrected_uvs[0].x, _corrected_uvs[0].y);
-                } else if(v.uv.x == 0.0f && v.uv.y == 1.0f) {
-                    // Top left
-                    o.uv = float2(_corrected_uvs[1].x, _corrected_uvs[1].y);
-                } else if(v.uv.x == 1.0f && v.uv.y == 0.0f) {
-                    // Bottom right
-                    o.uv = float2(_corrected_uvs[2].x, _corrected_uvs[2].y);
-                } else if(v.uv.x == 1.0f && v.uv.y == 1.0f) {
-                    // Top right
-                    o.uv = float2(_corrected_uvs[3].x, _corrected_uvs[3].y);
-                }
                 return o;
             }
             
             fixed4 frag (v2f i) : SV_Target
             {
-                // sample the texture
-                fixed4 col = tex2D(_MainTex, i.uv);
-                return col;
+                float3x3 dist_mat = (float3x3)_screen_distortion_matrix;
+                float3 normal_coordinate = float3(i.uv.x, i.uv.y, 1.0f);
+                float3 distored_coordinate = mul(normal_coordinate, dist_mat);
+                // Image is flipped so do 1-y to flip again
+                fixed4 col = tex2D(_MainTex, float2(distored_coordinate.x/distored_coordinate.z, 1-(distored_coordinate.y/distored_coordinate.z)));
+                return fixed4(col);
             }
             ENDCG
         }
